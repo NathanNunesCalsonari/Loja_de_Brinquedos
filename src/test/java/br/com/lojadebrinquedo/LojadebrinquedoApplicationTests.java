bb7cbb2612cdd383be1bb60b37a56ac906fcff55
Nathan Nunes Calsonari <130010914+NathanNunesCalsonari@users.noreply.github.com>
2024-10-25 20:27:53 -0300

    Add files via upload

--- src/test/java/br/com/lojadebrinquedo/LojadebrinquedoApplicationTests.java
+++ src/test/java/br/com/lojadebrinquedo/LojadebrinquedoApplicationTests.java
@@ -1,13 +1,25 @@
 package br.com.lojadebrinquedo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class LojadebrinquedoApplicationTests {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
 	@Test
 	void contextLoads() {
+		try {
+			jdbcTemplate.execute("SELECT 1");
+			System.out.println("Banco de dados Conectado");
+		} catch (Exception e) {
+			System.out.println("Banco de dados não conectado");
+		}
 	}
-
 }
